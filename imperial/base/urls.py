a4d1d2698f3e9b65874a5a3ba702cfb8781b89a3
pcampos <pcampos119104@gmail.com>
2024-02-07 10:25:25 -0300

    Adicionado home page

--- imperial/base/urls.py
+++ imperial/base/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
+from imperial.base.views import HomeView
+
+app_name = "base"
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('imperial.base.urls')),
+    path('', HomeView.as_view(), name='home'),
 ]
