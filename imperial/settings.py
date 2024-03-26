f0b176c2c5a110c7a1fb1d5f28feeebf9e518a15
pcampos <pcampos119104@gmail.com>
2024-03-26 11:11:53 -0300

    CI  On branch feature/19_ci  Changes to be committed: 	new file:   .github/workflows/ci.yml 	new file:   .github/workflows/deploy.yml 	modified:   .gitignore 	modified:   compose.yaml 	renamed:    compose/Dockerfile -> docker/dev/Dockerfile 	renamed:    compose/start -> docker/dev/start 	deleted:    imperial/base/tests.py 	new file:   imperial/base/tests/__init__.py 	new file:   imperial/base/tests/test_view.py 	modified:   imperial/settings.py 	modified:   justfile 	modified:   poetry.lock 	modified:   pyproject.toml

--- imperial/settings.py
+++ imperial/settings.py
@@ -65,6 +65,7 @@ if DEBUG:
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'imperial/static']
+
+STORAGES = {
+    # ...
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
